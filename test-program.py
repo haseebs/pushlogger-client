623dd67a71de42b3e32b8e2f6a49a51dfd48b45b
Haseeb Shah <haseebs@outlook.com>
2018-05-26 14:53:36 +0500

    Added log pusher script

--- test-program.py
+++ test-program.py
@@ -1,11 +1,13 @@
+import sys
 from time import sleep
 from random import random
 
-LINES = 10
-WAIT = 1
-OUT_STR = "Train Loss: {} Validation Loss: {} D(x): {} G(D(x)): {} Train Acc: {} Validation Acc: {}"
+LINES = 100
+WAIT = 0.5
+OUT_STR = "Train Loss: {} Validation Loss: {} D(x): {} G(D(x)): {}"
 
 for k in range(LINES):
-    print(OUT_STR.format(random(), random(), random(), random(), random(), random()))
+    print(OUT_STR.format(random(), random(), random(), random()))
+    sys.stdout.flush()
     sleep(WAIT)
 
